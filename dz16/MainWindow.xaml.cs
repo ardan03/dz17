46bbdd3b22e356f229efcd0e5e3185e30ff9f218
Ардан <@DESKTOP-5U9RBTE>
2023-10-19 10:25:28 +0800

    Добавьте файлы проекта.

---
+++ dz16/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dz16
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles= new List<string> {"light","dark"};
+            Thems.SelectionChanged += Thems_SelectionChanged;
+            Thems.ItemsSource= styles;
+            Thems.SelectedItem = "dark";
+        }
+
+        private void Thems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string style = Thems.SelectedItem as string;
+            var uri = new Uri(style + ".xaml",UriKind.Relative);
+            ResourceDictionary resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+    }
+}
